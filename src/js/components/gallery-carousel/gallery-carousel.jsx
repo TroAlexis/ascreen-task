f49843341820e6e3034d3d39a42273d063a57a1d
TroAlexis <aleksei.troshin1997@gmail.com>
2020-10-21 00:39:17 +0300

    Programmed and styled gallery-carousel

--- src/js/components/gallery-carousel/gallery-carousel.jsx
+++ src/js/components/gallery-carousel/gallery-carousel.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
 
 import './gallery-carousel.scss';
 
@@ -9,31 +11,45 @@ class GalleryCarousel extends React.Component {
 
     this.state = {
       currentSlide: 0,
+      fullscreen: false,
     };
   }
 
+  setCurrentSlide = (num) => this.setState({ currentSlide: num })
+
   render() {
-    const { slides } = this.props;
+    const {
+      slides, className, toggleFullScreen, fullscreen,
+    } = this.props;
     const { currentSlide } = this.state;
 
     return pug`
-      .gallery-carousel
+      .gallery-carousel(className=className)
         .gallery-carousel__wrapper
           .slides
-            each slide in slides
+            each slide,index in slides
               - const { url, alt } = slide;
-              img(src=require('Images/mountain-photos/' + url + '.png') alt=alt)
+              img(src=require('Images/mountain-photos/' + url + '.jpg') alt=alt styleName=currentSlide===index? "active": "" key=index)
               
           .controls
-            .controls__back
+            Link.controls__back(to="/")
             .controls__thumbs
               each slide, index in slides
-                span.controls__thumb(styleName=currentSlide===index? 'active': '')
+                span.controls__thumb(styleName=currentSlide===index? "active": "" onClick=() => this.setCurrentSlide(index) key=index)
                 
-            .controls__fullscreen
+            .controls__fullscreen(onClick=toggleFullScreen styleName=fullscreen? "fullscreen-open": "")
     `;
   }
 }
 
 export default GalleryCarousel;
-// gallery-carousel.propTypes = {}
+
+GalleryCarousel.propTypes = {
+  slides: PropTypes.arrayOf(PropTypes.object).isRequired,
+  className: PropTypes.string,
+  toggleFullScreen: PropTypes.func.isRequired,
+};
+
+GalleryCarousel.defaultProps = {
+  className: '',
+};
