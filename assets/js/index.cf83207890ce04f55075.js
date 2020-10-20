7ec4ec75dec83d064265cd0d0c2b1b3f5a52a9cc
TroAlexis <aleksei.troshin1997@gmail.com>
2020-10-21 00:42:59 +0300

    Updates

---
+++ assets/js/index.cf83207890ce04f55075.js
@@ -0,0 +1,232 @@
+!function(e){function r(r){for(var t,o,s=r[0],i=r[1],p=r[2],u=0,d=[];u<s.length;u++)o=s[u],Object.prototype.hasOwnProperty.call(n,o)&&n[o]&&d.push(n[o][0]),n[o]=0;for(t in i)Object.prototype.hasOwnProperty.call(i,t)&&(e[t]=i[t]);for(f&&f(r);d.length;)d.shift()();return c.push.apply(c,p||[]),a()}function a(){for(var e,r=0;r<c.length;r++){for(var a=c[r],t=!0,s=1;s<a.length;s++){var i=a[s];0!==n[i]&&(t=!1)}t&&(c.splice(r--,1),e=o(o.s=a[0]))}return e}var t={},n={1:0},c=[];function o(r){if(t[r])return t[r].exports;var a=t[r]={i:r,l:!1,exports:{}};return e[r].call(a.exports,a,a.exports,o),a.l=!0,a.exports}o.m=e,o.c=t,o.d=function(e,r,a){o.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:a})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(e,r){if(1&r&&(e=o(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var a=Object.create(null);if(o.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var t in e)o.d(a,t,function(r){return e[r]}.bind(null,t));return a},o.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(r,"a",r),r},o.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},o.p="/ascreen-task/";var s=window.webpackJsonp=window.webpackJsonp||[],i=s.push.bind(s);s.push=r,s=s.slice();for(var p=0;p<s.length;p++)r(s[p]);var f=i;c.push([24,2,0]),a()}(Array(20).concat([
+/*!*************************************************!*\
+  !*** ./src/assets/img/ui-icons/scroll-icon.svg ***!
+  \*************************************************/
+/*! exports provided: default, ReactComponent */
+/*! exports used: ReactComponent */function(e,r,a){"use strict";a.d(r,"a",(function(){return s}));var t=a(/*! react */0);function n(){return(n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var a=arguments[r];for(var t in a)Object.prototype.hasOwnProperty.call(a,t)&&(e[t]=a[t])}return e}).apply(this,arguments)}var c=t.createElement("path",{d:"M4.56 29L20.832 1.94 37.107 29H4.56z",stroke:"#87BDBA",strokeWidth:2}),o=t.createElement("path",{d:"M21.778 29l12.15-20.203L46.078 29h-24.3z",stroke:"#87BDBA",strokeWidth:2});function s(e){return t.createElement("svg",n({width:50,height:40,fill:"none"},e),c,o)}a.p},
+/*!******************************************************!*\
+  !*** ./src/assets/img/ui-icons/scroll-icon-full.svg ***!
+  \******************************************************/
+/*! exports provided: default, ReactComponent */
+/*! exports used: ReactComponent */function(e,r,a){"use strict";a.d(r,"a",(function(){return s}));var t=a(/*! react */0);function n(){return(n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var a=arguments[r];for(var t in a)Object.prototype.hasOwnProperty.call(a,t)&&(e[t]=a[t])}return e}).apply(this,arguments)}var c=t.createElement("path",{d:"M20.833 0l18.042 30H2.791L20.833 0z",fill:"#87BDBA"}),o=t.createElement("path",{d:"M33.928 6.857L47.847 30H20.01L33.928 6.857z",fill:"#87BDBA"});function s(e){return t.createElement("svg",n({width:50,height:40,fill:"none"},e),c,o)}a.p},,
+/*!************************************!*\
+  !*** ./src/entries/index/index.js ***!
+  \************************************/
+/*! no exports provided */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is an entry point */,function(e,r,a){"use strict";a.r(r);var t=a(/*! react */0),n=a.n(t),c=a(/*! react-dom */9),o=a.n(c),s=a(/*! react-router-dom */8),i=(a(/*! ../../assets/scss/global.scss */29),a(/*! Components/App/App */22));var p=document.querySelector(".root");o.a.render(n.a.createElement(s.a,null,n.a.createElement(i.a,null)),p)},,,,
+/*!*************************************!*\
+  !*** ./src/assets/scss/global.scss ***!
+  \*************************************/
+/*! no static exports found */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,r,a){},,,,,,,
+/*!**********************************************************!*\
+  !*** ./src/assets/img/mountain-icons sync ^\.\/.*\.png$ ***!
+  \**********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,r,a){var t={"./aikyivenchorr.png":38,"./chasnachorr.png":39,"./fersmana.png":40,"./kykisvymchorr.png":41,"./lyavochorr.png":42,"./tahtarvymchorr.png":43,"./udichvymchorr.png":44,"./vudiavrchorr.png":45};function n(e){var r=c(e);return a(r)}function c(e){if(!a.o(t,e)){var r=new Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}return t[e]}n.keys=function(){return Object.keys(t)},n.resolve=c,e.exports=n,n.id=37},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-icons/aikyivenchorr.png ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/aikyivenchorr-b82f6e078ff7514ec5de2dc9b849a94c.png"},
+/*!*******************************************************!*\
+  !*** ./src/assets/img/mountain-icons/chasnachorr.png ***!
+  \*******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/chasnachorr-7a442de8380ee1600b86086eedaff887.png"},
+/*!****************************************************!*\
+  !*** ./src/assets/img/mountain-icons/fersmana.png ***!
+  \****************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/fersmana-0377bb2022432a386cdd3d9415ae48fb.png"},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-icons/kykisvymchorr.png ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/kykisvymchorr-e3cdf191efe13487781155cf08486881.png"},
+/*!******************************************************!*\
+  !*** ./src/assets/img/mountain-icons/lyavochorr.png ***!
+  \******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/lyavochorr-9731e577b429bb9e31d8b1c2efa1a9eb.png"},
+/*!**********************************************************!*\
+  !*** ./src/assets/img/mountain-icons/tahtarvymchorr.png ***!
+  \**********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/tahtarvymchorr-3e9da0fe45276fd6eb82b85680e2d2f7.png"},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-icons/udichvymchorr.png ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/udichvymchorr-3c7c92da382cf9aa84b7b28a4c6241a1.png"},
+/*!********************************************************!*\
+  !*** ./src/assets/img/mountain-icons/vudiavrchorr.png ***!
+  \********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/vudiavrchorr-a967439e0c73d373a44906c5edcfef39.png"},,,,,,,,,,,,,,,,,,,,
+/*!***********************************************************!*\
+  !*** ./src/assets/img/mountain-photos sync ^\.\/.*\.jpg$ ***!
+  \***********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,r,a){var t={"./aikuavenchorr-2.jpg":67,"./aikyivenchorr-1.jpg":68,"./aikyivenchorr-2.jpg":69,"./aikyivenchorr-3.jpg":70,"./chasnachorr-1.jpg":71,"./chasnachorr-2.jpg":72,"./chasnachorr-3.jpg":73,"./fersmana-1.jpg":74,"./fersmana-2.jpg":75,"./fersmana-3.jpg":76,"./kykisvymchorr-1.jpg":77,"./kykisvymchorr-2.jpg":78,"./kykisvymchorr-3.jpg":79,"./lyavochorr-1.jpg":80,"./lyavochorr-2.jpg":81,"./lyavochorr-3.jpg":82,"./tahtarvymchorr-1.jpg":83,"./tahtarvymchorr-2.jpg":84,"./tahtarvymchorr-3.jpg":85,"./udichvymchorr-1.jpg":86,"./udichvymchorr-2.jpg":87,"./udichvymchorr-3.jpg":88,"./vudiavrchorr-1.jpg":89,"./vudiavrchorr-2.jpg":90,"./vudiavrchorr-3.jpg":91};function n(e){var r=c(e);return a(r)}function c(e){if(!a.o(t,e)){var r=new Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}return t[e]}n.keys=function(){return Object.keys(t)},n.resolve=c,e.exports=n,n.id=66},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/aikuavenchorr-2.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/aikuavenchorr-2-b19dc4fb38b5bdc026e156f2ee21ec3a.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/aikyivenchorr-1.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/aikyivenchorr-1-769636c0cbc5ee6c61d515f708f2116a.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/aikyivenchorr-2.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/aikyivenchorr-2-b19dc4fb38b5bdc026e156f2ee21ec3a.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/aikyivenchorr-3.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/aikyivenchorr-3-c0425e42416d11048d8f25d0d00584a0.jpg"},
+/*!**********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/chasnachorr-1.jpg ***!
+  \**********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/chasnachorr-1-8eee05c2d74edf84bb2fa428a3a57dfd.jpg"},
+/*!**********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/chasnachorr-2.jpg ***!
+  \**********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/chasnachorr-2-c3c255ab7f6f3d252093ca47f245b415.jpg"},
+/*!**********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/chasnachorr-3.jpg ***!
+  \**********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/chasnachorr-3-50ebfa992f28f5c6d3d59378ebfdbe90.jpg"},
+/*!*******************************************************!*\
+  !*** ./src/assets/img/mountain-photos/fersmana-1.jpg ***!
+  \*******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/fersmana-1-c911cef9ffd682bcd4b8614c934f32ba.jpg"},
+/*!*******************************************************!*\
+  !*** ./src/assets/img/mountain-photos/fersmana-2.jpg ***!
+  \*******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/fersmana-2-f2ec54ee8639020501e98e7a2f5b41a2.jpg"},
+/*!*******************************************************!*\
+  !*** ./src/assets/img/mountain-photos/fersmana-3.jpg ***!
+  \*******************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/fersmana-3-caea139c9c4176b64d0a534b8fb32f2b.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/kykisvymchorr-1.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/kykisvymchorr-1-247abe0ff31465e567ff2d14cd00acfc.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/kykisvymchorr-2.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/kykisvymchorr-2-7ca661d700f53a088d943d262703e2a6.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/kykisvymchorr-3.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/kykisvymchorr-3-9df5015cdf2a34346f2cdf4b9315ef40.jpg"},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/lyavochorr-1.jpg ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/lyavochorr-1-aca250ee7994077c0bacc1935a451452.jpg"},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/lyavochorr-2.jpg ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/lyavochorr-2-b690acda76afd27a0063459e0cc8461b.jpg"},
+/*!*********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/lyavochorr-3.jpg ***!
+  \*********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/lyavochorr-3-42f02725bdc0f7183a14857a454782e5.jpg"},
+/*!*************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/tahtarvymchorr-1.jpg ***!
+  \*************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/tahtarvymchorr-1-dddd6ae4498596e7b6d30726044dc1ec.jpg"},
+/*!*************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/tahtarvymchorr-2.jpg ***!
+  \*************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/tahtarvymchorr-2-1d187475cba99f5cfc39677f47483c09.jpg"},
+/*!*************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/tahtarvymchorr-3.jpg ***!
+  \*************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/tahtarvymchorr-3-548abd610943644fb66ef33aef867e73.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/udichvymchorr-1.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/udichvymchorr-1-6e70ba9e9734aa998d783c16a8c8f26e.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/udichvymchorr-2.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/udichvymchorr-2-88857c4b6409828461beaf6ec5659707.jpg"},
+/*!************************************************************!*\
+  !*** ./src/assets/img/mountain-photos/udichvymchorr-3.jpg ***!
+  \************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/udichvymchorr-3-972cb254d71da9d6012789dccbbde6e9.jpg"},
+/*!***********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/vudiavrchorr-1.jpg ***!
+  \***********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/vudiavrchorr-1-6b1ae83ca1c8315cbb420d0ed1456b2d.jpg"},
+/*!***********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/vudiavrchorr-2.jpg ***!
+  \***********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/vudiavrchorr-2-31849693bd988249e03444c8dddd2c50.jpg"},
+/*!***********************************************************!*\
+  !*** ./src/assets/img/mountain-photos/vudiavrchorr-3.jpg ***!
+  \***********************************************************/
+/*! no static exports found */
+/*! all exports used */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */function(e,r,a){e.exports=a.p+"assets/images/vudiavrchorr-3-474a9ff6746ee49f9f0d289961e1fd05.jpg"}]));
